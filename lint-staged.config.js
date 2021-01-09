1f8eb929e5addba1e9f98504ac73fa75e67d7944
Thilo Maier <git@maier.asia>
2021-01-09 21:29:21 +0000

    Fix lint-staged.config.js Fixes #190

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,16 +7,25 @@ const prettierSupportedExtensions = prettier
   .languages.map(({ extensions }) => extensions)
   .flat();
 
+const addQuotes = (a) => `"${a}"`;
+
 module.exports = (allStagedFiles) => {
-  // Match files for ESLint.
-  const eslintFiles = micromatch(allStagedFiles, ['**/*.js']);
-  // Match all files that can be formated by Prettier.
+  // Match files for ESLint including dirs and files starting with dot.
+  const eslintFiles = micromatch(allStagedFiles, ['**/*.js'], { dot: true });
+  // Match for Prettier including dirs and files starting with dot.
   const prettierFiles = micromatch(
     allStagedFiles,
-    prettierSupportedExtensions.map((extension) => `**/*${extension}`)
+    prettierSupportedExtensions.map((extension) => `**/*${extension}`),
+    { dot: true }
   );
-  return [
-    `eslint --fix ${eslintFiles.join(' ')}`,
-    `prettier --write ${prettierFiles.join(' ')}`,
-  ];
+
+  // Array of linters to be run in this sequence.
+  const linters = [];
+  // Only add linters when there are staged files for them.
+  // `prettier --write` makes lint-staged never terminate if prettierFiles is empty.
+  if (eslintFiles.length > 0)
+    linters.push(`eslint --fix ${eslintFiles.join(' ')}`);
+  if (prettierFiles.length > 0)
+    linters.push(`prettier --write ${prettierFiles.map(addQuotes).join(' ')}`);
+  return linters;
 };
